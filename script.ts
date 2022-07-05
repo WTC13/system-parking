63a6de077a336ad18541afc97f8f4c24b4603605
WTC13 <wendell.timoteoc@gmail.com>
2022-07-05 05:12:24 -0300

    main project

--- script.ts
+++ script.ts
@@ -1,5 +1,37 @@
+interface veiculo{
+    nome: string,
+    placa: string,
+    entrada: Date;
+}
+
 (function(){
     const $ = (query: string): HTMLInputElement | null => document.querySelector(query);
+
+        function patio() {
+            function ler(){}
+
+            function adicionar(veiculo: veiculo){
+                const row = document.createElement("tr");
+                row.innerHTML = `
+                    <td>${veiculo.nome}</td>
+                    <td>${veiculo.placa}</td>
+                    <td>${veiculo.entrada}</td>
+                    <td>
+                        <button class="delete" data-placa="${veiculo.placa}">X</button>
+                    </td>
+                `;
+
+                $("#patio")?.appendChild(row);
+            }
+
+            function remover(){}
+
+            function salvar(){}
+
+            function render(){}
+
+            return{ ler, adicionar, remover, salvar, render };
+        }
 
     $("#cadastrar")?.addEventListener("click", () =>{
         const nome = $("#nome")?.value;
@@ -9,5 +41,7 @@
             alert("Os campos nome e placa são obrigatórios");
             return false;
         }
+
+        patio().adicionar({nome, placa, entrada: new Date() });
     })
 })();
